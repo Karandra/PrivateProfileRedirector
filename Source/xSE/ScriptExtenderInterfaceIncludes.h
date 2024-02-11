ac74896d4b9cf491223067ccc31edafc49a3f6a4
Karandra <32564546+Karandra@users.noreply.github.com>
2024-02-11 21:34:27 +0300

    Updated xSE binaries references

--- Source/xSE/ScriptExtenderInterfaceIncludes.h
+++ Source/xSE/ScriptExtenderInterfaceIncludes.h
@@ -23,13 +23,16 @@
 #include <skse64/GameAPI.h>
 #include <skse64/ObScript.h>
 
-#pragma comment(lib, "skse64/x64_v142/Release_VC142/common_vc14.lib")
-#pragma comment(lib, "skse64/x64/Release_VC142/skse64_common.lib")
-#pragma comment(lib, "skse64/x64/Release/skse64_loader_common.lib")
 #if xSE_PLATFORM_SKSE64
-#pragma comment(lib, "skse64/x64/Release_Lib_VC142/skse64_1_5_97.lib")
+#pragma comment(lib, "skse64/x64/Release/skse64_1_5_97.lib")
+#pragma comment(lib, "skse64/x64_v143/Release/common_vc14.lib")
+#pragma comment(lib, "skse64/x64/Release/skse64_common.lib")
+#pragma comment(lib, "skse64/x64/Release/skse64_loader_common.lib")
 #elif xSE_PLATFORM_SKSE64AE
 #pragma comment(lib, "skse64/x64/Release_Lib_VC142/skse64_1_6_323.lib")
+#pragma comment(lib, "skse64/x64_v143/Release/common_vc14.lib")
+#pragma comment(lib, "skse64/x64/Release/skse64_common.lib")
+#pragma comment(lib, "skse64/x64/Release/skse64_loader_common.lib")
 #endif
 
 #elif xSE_PLATFORM_SKSEVR
@@ -45,7 +48,7 @@
 #pragma comment(lib, "skseVR/x64/Release_Lib_VC142/sksevr_1_4_15.lib")
 #pragma comment(lib, "skseVR/x64/Release_VC142/skse64_common.lib")
 #pragma comment(lib, "skseVR/x64/Release/skse64_loader_common.lib")
-#pragma comment(lib, "skseVR/x64_v142/Release_VC142/common_vc14.lib")
+#pragma comment(lib, "skseVR/x64_v143/Release_VC142/common_vc14.lib")
 
 #elif xSE_PLATFORM_F4SE
 
@@ -60,7 +63,7 @@
 #pragma comment(lib, "f4se/x64/Release/f4se_1_10_163.lib")
 #pragma comment(lib, "f4se/x64/Release/f4se_common.lib")
 #pragma comment(lib, "f4se/x64/Release/f4se_loader_common.lib")
-#pragma comment(lib, "f4se/x64_vc11/Release/common_vc11.lib")
+#pragma comment(lib, "f4se/x64_v143/Release/common_vc14.lib")
 
 #elif xSE_PLATFORM_F4SEVR
 
